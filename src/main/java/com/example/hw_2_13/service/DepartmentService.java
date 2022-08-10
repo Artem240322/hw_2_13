95bc24ff1e0425dd221583baaa64015fe7c60d7d
ArtemCH240322 <sun2383@mail.ru>
2022-08-10 14:39:30 +0300

    Первый комит. Добавляю Controller. Добавляю Service/

--- src/main/java/com/example/hw_2_13/service/DepartmentService.java
+++ src/main/java/com/example/hw_2_13/service/DepartmentService.java
@@ -2,8 +2,45 @@ package com.example.hw_2_13.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Service
 public class DepartmentService {
+    private final EmployeeService employeeService;
+
+    public final DepartmentService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    public Employee employeeWithMaxSalary(int departmentId) {
+        return employeeService.getAll().stream()
+                .filter(employee -> employee.getDepartment() == departmentId)
+                .max(Character.comparingDouble(Employee::getSalary))
+                .orElseThrow(EmployeeNotFoundException::new);
+    }
+
+    public Employee employeeWithMinSalary(int departmentId) {
+        return employeeService.getAll().stream()
+                .filter(employee -> employee.getDepartment() == departmentId)
+                .min(Comparator.comparingDouble(Employee::getSalary))
+                .orElseThrow(EmployeeNotFoundException::new);
+    }
+
+    public List<Employee> employeesFromDepartment(int departmentId) {
+        return employeeService.getAll().stream()
+                .filter(employee -> employee.getDepartment() == departmentId)
+                .collect(Collectors.toList());
+    }
+
+
+
+    public Map<Integer, List<Employee>> employeesGroupedByDepartment() {
+        return employeeService.getAll().stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+    }
 
 
 }
